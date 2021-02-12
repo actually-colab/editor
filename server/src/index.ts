cf55902f9db693f729dd3695a0b25d186727d16a
Bailey Tincher <btincher99@gmail.com>
2021-02-11 23:01:29 -0600

    Add dynamodb connection sample in test route

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,26 @@ import type {
 import { ShallotAWSRestWrapper } from '@shallot/rest-wrapper';
 import createHTTPError from 'http-errors';
 
+import db from './db/connection';
+import tablenames from './db/tables';
+
 type TEvent = TShallotHttpEvent<{ username: string }>;
 
 const _handler: ShallotRawHandler<TEvent, { username: string }> = async ({
   queryStringParameters,
 }) => {
+  const result = await db.docClient
+    .put({ Item: { uid: 'test' }, TableName: tablenames.usersTableName })
+    .promise();
+
+  console.log(result);
+
+  const result2 = await db.docClient
+    .scan({ TableName: tablenames.usersTableName })
+    .promise();
+
+  console.log(result, result2);
+
   if (queryStringParameters?.username == null) {
     throw new createHTTPError.BadRequest('Must specify username!');
   }
