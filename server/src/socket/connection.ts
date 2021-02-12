e89534ddea819e1f871cfc67db5a6cbd5a548a87
Bailey Tincher <btincher99@gmail.com>
2021-02-11 21:23:06 -0600

    Add socket connect/disconnect (#21)
    * Add connect/disconnect/default base for socket server
    
    * Fix naming

---
+++ server/src/socket/connection.ts
@@ -0,0 +1,25 @@
+import type { Handler, APIGatewayEvent } from 'aws-lambda';
+
+const SocketEventTypes = Object.freeze({
+  Connect: 'Connect',
+  Disconnect: 'Disconnect',
+});
+
+const success = {
+  statusCode: 200,
+};
+
+const error = {
+  statusCode: 400,
+};
+
+export const handler: Handler = async (event: APIGatewayEvent) => {
+  switch (event.requestContext.eventType) {
+    case SocketEventTypes.Connect:
+      return success;
+    case SocketEventTypes.Disconnect:
+      return success;
+    default:
+      return error;
+  }
+};
