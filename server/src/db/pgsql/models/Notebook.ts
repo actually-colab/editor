b025298cd5d30459866ff2b7409b82a1d023f2d3
Bailey Tincher <btincher99@gmail.com>
2021-02-18 17:50:44 -0600

    Add getNotebooksForUser

--- server/src/db/pgsql/models/Notebook.ts
+++ server/src/db/pgsql/models/Notebook.ts
@@ -3,12 +3,16 @@ import type { DUser } from './User';
 import pgsql from '../connection';
 import tablenames from '../tablenames';
 
-import { grantAccess } from './NotebookAccessLevel';
+import { DNotebookAccessLevel, grantAccess } from './NotebookAccessLevel';
 
 export interface DNotebook {
   nb_id: number;
   name: string;
   language: 'python2' | 'python3';
+}
+
+export interface Notebook extends DNotebook {
+  users: (DUser & { access_level: DNotebookAccessLevel['access_level'] })[];
 }
 
 export const createNotebook = async (
@@ -27,4 +31,35 @@ export const createNotebook = async (
   });
 
   return notebookRecord;
+};
+
+export const getNotebooksForUser = async (email: DUser['email']): Promise<Notebook[]> => {
+  return pgsql
+    .select(
+      'nb.*',
+      `json_agg(
+      json_build_object(
+        'uid', u.uid, 
+        'email', u.email, 
+        'name', u.name, 
+        'access_level', nba.access_level
+      )
+    ) AS users`
+    )
+    .from({ nb: tablenames.notebooksTableName })
+    .innerJoin(
+      { nba: tablenames.notebookAccessLevelsTableName },
+      'nba.nb_id',
+      '=',
+      'nb.nb_id'
+    )
+    .innerJoin({ u: tablenames.usersTableName }, 'u.uid', '=', 'nba.uid')
+    .whereIn(
+      'nb.nb_id',
+      pgsql
+        .select('nb_id')
+        .from({ sub_nba: tablenames.notebookAccessLevelsTableName })
+        .innerJoin({ sub_u: tablenames.usersTableName }, 'sub_u.uid', '=', 'sub_nba.uid')
+        .where({ 'sub_u.email': email })
+    );
 };
