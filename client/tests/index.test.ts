a1b5e99a129d4c93e11de31ab7b88d7bdf0eb41f
Bailey Tincher <btincher99@gmail.com>
2021-02-18 21:46:09 -0600

    Add module starter

---
+++ client/tests/index.test.ts
@@ -0,0 +1,9 @@
+import { test, describe, expect } from '@jest/globals';
+
+import Sample from '../src';
+
+describe('', () => {
+  test('', () => {
+    expect(Sample).toBeTruthy();
+  });
+});
---
+++ client/webpack.config.ts
@@ -0,0 +1,32 @@
+import type { Configuration } from 'webpack';
+
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+
+const config: Configuration = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    index: './src/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  externals: [nodeExternals()],
+};
+
+export default config;
