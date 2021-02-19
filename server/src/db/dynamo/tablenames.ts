d5620b02a4285344f1682c57ae594e967e2c35e1
Bailey Tincher <btincher99@gmail.com>
2021-02-18 18:33:00 -0600

    Remove dynamo for User/Notebook tables

--- server/src/db/dynamo/tablenames.ts
+++ server/src/db/dynamo/tablenames.ts
@@ -1,6 +1,4 @@
 export default {
-  usersTableName: process.env.AC_DDB_USERS_TABLE_NAME ?? 'User',
-  notebooksTableName: process.env.AC_DDB_NOTEBOOKS_TABLE_NAME ?? 'Notebook',
   cellsTableName: process.env.AC_DDB_CELLS_TABLE_NAME ?? 'Cell',
   outputsTableName: process.env.AC_DDB_OUTPUTS_TABLE_NAME ?? 'Output',
 };
