d8e481fcfa9c9d12f09ab3d53a70f11633148a22
Bailey Tincher <btincher99@gmail.com>
2021-03-08 01:36:28 -0600

    Create GET notebook/:id endpoint

---
+++ server/src/rest/get-notebook-contents.ts
@@ -0,0 +1,55 @@
+import type {
+  ShallotRawHandler,
+  TShallotHttpEvent,
+} from '@shallot/rest-wrapper/dist/aws';
+
+import type { DUser } from '../db/pgsql/models/User';
+import { getNotebookContents, NotebookContents } from '../db/pgsql/models/Notebook';
+
+import { ShallotAWSRestWrapper } from '@shallot/rest-wrapper';
+import createHTTPError from 'http-errors';
+
+import {
+  DNotebookAccessLevel,
+  getUserAccessLevel,
+} from '../db/pgsql/models/NotebookAccessLevel';
+
+type TEvent = TShallotHttpEvent<
+  unknown,
+  { nb_id: DNotebookAccessLevel['nb_id'] },
+  unknown,
+  unknown
+>;
+
+const _handler: ShallotRawHandler<TEvent, NotebookContents> = async ({
+  requestContext: { authorizer },
+  pathParameters,
+}) => {
+  const user = authorizer as DUser | null;
+  if (user?.email == null) {
+    throw new createHTTPError.InternalServerError();
+  }
+
+  if (pathParameters?.nb_id == null) {
+    throw new createHTTPError.BadRequest('Must specify body.nb_id');
+  }
+
+  const requestingUserAccessLevel = await getUserAccessLevel(
+    user.email,
+    pathParameters.nb_id
+  );
+  if (requestingUserAccessLevel == null) {
+    throw new createHTTPError.Forbidden('Does not have access to notebook');
+  }
+
+  const notebook = await getNotebookContents(pathParameters.nb_id);
+  if (notebook == null) {
+    throw new createHTTPError.InternalServerError('Could not load notebook');
+  }
+
+  return { message: 'success', data: notebook };
+};
+
+export const handler = ShallotAWSRestWrapper(_handler, undefined, {
+  HttpErrorHandlerOpts: { catchAllErrors: true },
+});
