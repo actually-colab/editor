425d4ef10bbf7da23e713d229b02631b097d42c5
Bailey Tincher <btincher99@gmail.com>
2021-03-08 18:00:35 -0600

    Add create_cell socket event

--- server/src/db/pgsql/models/Cell.ts
+++ server/src/db/pgsql/models/Cell.ts
@@ -39,3 +39,34 @@ export const editCell = async (
       }),
   ]);
 };
+
+export const createCell = async (
+  session: DActiveSession,
+  newCell: Partial<DCell>
+): Promise<DCell | null> => {
+  const promises: [unknown, Promise<DCell[] | null>] = [
+    pgsql<DActiveSession>(tablenames.activeSessionsTableName)
+      .update({
+        last_event: Date.now(),
+      })
+      .whereNull('time_disconnected')
+      .andWhere({
+        connectionId: session.connectionId,
+        nb_id: session.nb_id,
+      }),
+    pgsql<DCell>(tablenames.cellsTableName)
+      .insert({
+        contents: '',
+        ...newCell,
+        time_modified: Date.now(),
+      })
+      .returning('*'),
+  ];
+  const res = await Promise.all(promises);
+
+  if (res[1] == null || res[1].length === 0) {
+    return null;
+  }
+
+  return res[1][0];
+};
