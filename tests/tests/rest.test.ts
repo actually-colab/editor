51f08cba9f0a775b5de3787ec4e84d9c2520c491
Bailey Tincher <btincher99@gmail.com>
2021-03-19 14:54:36 -0500

    Add Create/Share Notebook tests

--- tests/tests/rest.test.ts
+++ tests/tests/rest.test.ts
@@ -1,8 +1,13 @@
 import { test, describe, expect } from '@jest/globals';
 
 import { v4 as uuid } from 'uuid';
+import _ from 'lodash';
 
-import { ActuallyColabRESTClient, DUser } from '@actually-colab/editor-client';
+import {
+  ActuallyColabRESTClient,
+  DUser,
+  NotebookAccessLevelType,
+} from '@actually-colab/editor-client';
 
 const getTestUser = async (): Promise<{
   apiClient: ActuallyColabRESTClient;
@@ -41,5 +46,64 @@ describe('Login', () => {
     expect(demoNotebook.users[0]).toMatchObject({ ...user, access_level: 'Full Access' });
 
     expect(Object.keys(demoNotebook.cells)).toHaveLength(7);
+  });
+});
+
+describe('Notebook', () => {
+  test('Create Notebook', async () => {
+    const { apiClient, user } = await getTestUser();
+
+    const expectedTestNotebook = {
+      name: 'Test Notebook',
+      language: 'python',
+    };
+    const newNotebook = await apiClient.createNotebook(expectedTestNotebook.name);
+    expect(newNotebook).toMatchObject(expectedTestNotebook);
+    expect(newNotebook.users).toEqual([
+      expect.objectContaining((user as unknown) as Record<string, unknown>),
+    ]);
+
+    const notebooks = await apiClient.getNotebooksForUser();
+    expect(notebooks).toHaveLength(2);
+    expect(notebooks).toEqual(
+      expect.arrayContaining([expect.objectContaining(expectedTestNotebook)])
+    );
+  });
+
+  test('Share Notebook', async () => {
+    const accessLevels: NotebookAccessLevelType[] = [
+      'Full Access',
+      'Full Access',
+      'Read Only',
+      'Read Only',
+      'Full Access',
+    ];
+
+    const testUsers = await Promise.all(accessLevels.map(() => getTestUser()));
+    const mainUser = testUsers[0];
+
+    const expectedUserAccessLevels = _.zip(testUsers, accessLevels).map(
+      ([user, access_level]) => ({
+        ...user?.user,
+        access_level,
+      })
+    );
+
+    const newNotebook = await mainUser.apiClient.createNotebook('Test Notebook');
+    await Promise.all(
+      expectedUserAccessLevels.slice(1).map((ual) =>
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        mainUser.apiClient.shareNotebook(ual.email!, newNotebook.nb_id, ual.access_level!)
+      )
+    );
+
+    const sharedNotebook = await mainUser.apiClient.getNotebookContents(
+      newNotebook.nb_id
+    );
+    expect(sharedNotebook.users).toEqual(
+      expect.arrayContaining(
+        expectedUserAccessLevels.map((ual) => expect.objectContaining(ual))
+      )
+    );
   });
 });
