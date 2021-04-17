9c20b48dab07ab5900b84232c6d6afe3a873e9bc
Bailey Tincher <btincher99@gmail.com>
2021-04-17 10:24:26 -0500

    Fix jest offline runner (#196)
    * Remove force exit from tests
    
    * Overhaul local test runner
    
    * rename test runner for clarity

---
+++ tests/run-tests.ts
@@ -0,0 +1,63 @@
+import { ChildProcessWithoutNullStreams, spawn, execSync } from 'child_process';
+import kill from 'tree-kill';
+
+let serverProcess: ChildProcessWithoutNullStreams;
+
+const sleep = async (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const startSLSOffline = (): Promise<unknown> =>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  new Promise<void>((resolve, _reject) => {
+    serverProcess = spawn('yarn', ['start'], { cwd: '../server' });
+
+    console.log(`Serverless: Offline started with PID : ${serverProcess.pid}`);
+
+    serverProcess.stdout.on('data', (data) => {
+      if (data.includes('[HTTP] server ready')) {
+        console.log(data.toString().trim());
+        resolve();
+      }
+    });
+
+    serverProcess.stderr.on('data', (errData: Buffer) => {
+      console.error(errData.toString());
+    });
+  });
+
+const killSLSOffline = (): void => {
+  if (serverProcess) {
+    serverProcess.stdout.pause();
+    serverProcess.stderr.pause();
+    kill(serverProcess.pid);
+    console.log('Serverless: Offline stopped');
+  } else {
+    throw new Error(`Could not kill Serverless Offline`);
+  }
+};
+
+const runTest = async (): Promise<void> => {
+  await startSLSOffline();
+
+  let status = 0;
+  try {
+    let args = '';
+    if (process.argv.length > 2) {
+      args = process.argv.slice(2).join(' ');
+    }
+
+    execSync(`yarn jest ${args}`, { stdio: 'inherit' });
+  } catch (err) {
+    console.error(err);
+    status = err.status ?? 1;
+  }
+
+  await sleep(1000);
+  killSLSOffline();
+
+  await sleep(1000);
+  process.exit(status);
+};
+
+runTest();
