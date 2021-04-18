981bdaa9c8020421f1f7c970fa6fc1bf7a41f5f6
Bailey Tincher <btincher99@gmail.com>
2021-04-17 20:08:35 -0500

    Fix types in webpack config

--- server/webpack.config.ts
+++ server/webpack.config.ts
@@ -20,7 +20,6 @@ const config: Configuration = {
   },
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
