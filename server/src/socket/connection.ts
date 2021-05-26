ad8f49dfd6b73ae4cd6ebaa4c352df705cc12eaa
Bailey Tincher <btincher99@gmail.com>
2021-05-26 15:46:12 -0400

    decompose socket connection handlers

--- server/src/socket/connection.ts
+++ server/src/socket/connection.ts
@@ -5,11 +5,6 @@ import { getUserFromBearerToken } from '../authorizer/token';
 import { connect, disconnect } from '../db/pgsql/models/ActiveSession';
 import { broadcastToNotebook, forceDisconnect } from './client-management';
 
-const SocketEventTypes = Object.freeze({
-  Connect: 'CONNECT',
-  Disconnect: 'DISCONNECT',
-});
-
 const success = {
   statusCode: 200,
 };
@@ -27,81 +22,95 @@ type APIGatewayWebSocketEvent = APIGatewayProxyEvent & {
   requestContext: WebSocketRequestContext;
 };
 
-export const handler: Handler = async (event: APIGatewayWebSocketEvent) => {
-  switch (event.requestContext.eventType) {
-    case SocketEventTypes.Connect: {
-      // SLS Offline doesn't support Lambda Authorizers :,(
-      if (process.env.IS_OFFLINE != null) {
-        const bearerToken =
-          event.headers.Authorization ?? event.queryStringParameters?.sessionToken;
-        if (bearerToken == null) {
-          console.error(
-            event.requestContext.connectionId,
-            'Authorization token not supplied... forcing disconnect...'
-          );
-          await forceDisconnect(event.requestContext);
-          return { statusCode: 401 };
-        }
-
-        const user = await getUserFromBearerToken(bearerToken);
-        if (user == null) {
-          console.error(
-            event.requestContext.connectionId,
-            'Authorization token invalid... forcing disconnect...'
-          );
-          await forceDisconnect(event.requestContext);
-          return { statusCode: 401 };
-        }
-
-        event.requestContext.authorizer = user;
-      }
-
-      const newSession: DActiveSession = {
-        connectionId: event.requestContext.connectionId,
-        uid: event.requestContext.authorizer.uid,
-        time_connected: event.requestContext.requestTimeEpoch,
-        last_event: event.requestContext.requestTimeEpoch,
-      };
-
-      await connect(newSession);
-
-      return success;
-    }
-    case SocketEventTypes.Disconnect: {
-      const { disconnectedSessions, unlockedCells } = await disconnect(
-        event.requestContext.connectionId,
-        event.requestContext.requestTimeEpoch
-      );
-
-      if (disconnectedSessions.length > 0) {
-        const uid = disconnectedSessions[0].uid;
-
-        await Promise.all([
-          Promise.all(
-            disconnectedSessions.map((session) =>
-              broadcastToNotebook(event.requestContext, session.nb_id, {
-                action: 'notebook_closed',
-                triggered_by: session.uid,
-                data: { nb_id: session.nb_id, uid: session.uid },
-              })
-            )
-          ),
-          Promise.all(
-            unlockedCells.map((cell) =>
-              broadcastToNotebook(event.requestContext, cell.nb_id, {
-                action: 'cell_unlocked',
-                triggered_by: uid,
-                data: cell,
-              })
-            )
-          ),
-        ]);
-      }
-
-      return success;
-    }
-    default:
-      console.error('unknown request', event);
-      return error;
+/**Simulates the lambda authorizer since SLS Offline does not support
+ * them.
+ *
+ * @param event APIGateway Event
+ * @returns the user or null
+ */
+const authorizeUserForOffline = async (
+  event: APIGatewayWebSocketEvent
+): Promise<DUser | null> => {
+  const bearerToken =
+    event.headers.Authorization ?? event.queryStringParameters?.sessionToken;
+  if (bearerToken == null) {
+    console.error(
+      event.requestContext.connectionId,
+      'Authorization token not supplied... forcing disconnect...'
+    );
+    await forceDisconnect(event.requestContext);
+    return null;
   }
+
+  const user = await getUserFromBearerToken(bearerToken);
+  if (user == null) {
+    console.error(
+      event.requestContext.connectionId,
+      'Authorization token invalid... forcing disconnect...'
+    );
+    await forceDisconnect(event.requestContext);
+    return null;
+  }
+
+  return user;
+};
+
+export const connectHandler: Handler<APIGatewayWebSocketEvent> = async (event) => {
+  if (process.env.IS_OFFLINE != null) {
+    const user = await authorizeUserForOffline(event);
+    if (user == null) {
+      return { statusCode: 401 };
+    }
+    event.requestContext.authorizer = user;
+  }
+
+  const newSession: DActiveSession = {
+    connectionId: event.requestContext.connectionId,
+    uid: event.requestContext.authorizer.uid,
+    time_connected: event.requestContext.requestTimeEpoch,
+    last_event: event.requestContext.requestTimeEpoch,
+  };
+
+  await connect(newSession);
+
+  return success;
+};
+
+export const disconnectHandler: Handler<APIGatewayWebSocketEvent> = async (event) => {
+  const { disconnectedSessions, unlockedCells } = await disconnect(
+    event.requestContext.connectionId,
+    event.requestContext.requestTimeEpoch
+  );
+
+  if (disconnectedSessions.length > 0) {
+    const uid = disconnectedSessions[0].uid;
+
+    await Promise.all([
+      Promise.all(
+        disconnectedSessions.map((session) =>
+          broadcastToNotebook(event.requestContext, session.nb_id, {
+            action: 'notebook_closed',
+            triggered_by: session.uid,
+            data: { nb_id: session.nb_id, uid: session.uid },
+          })
+        )
+      ),
+      Promise.all(
+        unlockedCells.map((cell) =>
+          broadcastToNotebook(event.requestContext, cell.nb_id, {
+            action: 'cell_unlocked',
+            triggered_by: uid,
+            data: cell,
+          })
+        )
+      ),
+    ]);
+  }
+
+  return success;
+};
+
+export const defaultHandler: Handler = async (event) => {
+  console.error('Unknown request:', event);
+  return error;
 };
